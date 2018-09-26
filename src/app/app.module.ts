8f2d5f0aa8fa128fac10cd1a12bff1284fd8a697
YaroslavVorochuk <38429499+YaroslavVorochuk@users.noreply.github.com>
2018-09-26 11:15:53 +0300

    Start
    First commit, added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { AddressListComponent } from './address-list/address-list.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    AddressListComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
