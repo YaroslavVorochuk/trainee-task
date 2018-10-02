d137b3a1394de7f5f24030cd48bdd7515e24ab6b
YaroslavVorochuk <38429499+YaroslavVorochuk@users.noreply.github.com>
2018-10-02 23:05:19 +0300

    added maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { AgmCoreModule } from '@agm/core';
+
+
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { AddressListComponent } from './address-list/address-list.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
     AddressListComponent,
-    NavigationComponent
+    NavigationComponent,
+    ContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDg6Sr7Qvx35qzI_9sLHkkBbJ-AtxFxxKg'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  latitude = 49.85;
+  longitude = 24.0166666667;
 
+  onMapMarker(event) {
+    console.log(event);
+  }
   constructor() { }
 
   ngOnInit() {
